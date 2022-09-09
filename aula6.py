d3f5a4c6c006df21f64050992ecac8bb4a89c0aa
mistickesterio <thiagosilvadacruz163@gmail.com>
2022-09-09 00:34:57 -0300

    first commit

---
+++ aula6.py
@@ -0,0 +1,14 @@
+print ("vamos fazer uma conta?")
+
+operacao = input("diga a operação")
+num1 = int(input("diga o primeiro número"))
+num2 = int(input("diga o segundo número"))
+
+if operacao = '+':
+
+soma = (num1 + num2)
+
+if soma <= 0:
+    print ("negative")
+else:
+    print ("positive")
